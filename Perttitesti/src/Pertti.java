a26a8d27486e67c910b68f89d177451110f248fa
Inka <inka.haltiapuu2@myy.haaga-helia.fi>
2015-09-29 08:57:06 +0300

    testi8

--- Perttitesti/src/Pertti.java
+++ Perttitesti/src/Pertti.java
@@ -1,9 +1,10 @@
 
 public class Pertti {
-//testihommia
+//testihommia ja testihommia
 	public static void main(String[] args) {
 		System.out.println("Pertsa is bestest!");
 		System.out.println("..vai onko?!???");
+		System.out.println("?!?!!?)");
 		System.out.println("Se on viel‰ ep‰selv‰‰.");
 
 	}
