195f2ab8f137ab1be8ed442e994bf51772350199
Katja <a1204023@myy.haaga-helia.fi>
2015-09-29 08:25:18 +0300

    Testing5

--- Perttitesti/src/Pertti.java
+++ Perttitesti/src/Pertti.java
@@ -1,9 +1,9 @@
 
 public class Pertti {
-
+//testihommia
 	public static void main(String[] args) {
 		System.out.println("Pertti is bestest!");
-		System.out.println("..vai onko???");
+		System.out.println("..vai onko?!???");
 
 	}
 
